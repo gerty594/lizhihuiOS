2f9d3138132f2a2357c7fe1efb67a350baf23431
gerty594 <969984561@qq.com>
2022-12-09 21:58:45 +0800

    20301134_lab6_step7

--- os/src/mm/memory_set.rs
+++ os/src/mm/memory_set.rs
@@ -30,6 +30,13 @@ extern "C" {
     fn strampoline();
 }
 
+lazy_static! {
+    pub static ref KERNEL_SPACE: Arc<UPSafeCell<MemorySet>> = Arc::new(unsafe {
+        UPSafeCell::new(MemorySet::new_kernel()
+    )});
+}
+
+
 impl MemorySet {
     pub fn new_kernel() -> Self {
         let mut memory_set = Self::new_bare();
@@ -286,4 +293,23 @@ impl MapArea {
     }
 }
 
-
+#[allow(unused)]
+pub fn remap_test() {
+    let mut kernel_space = KERNEL_SPACE.exclusive_access();
+    let mid_text: VirtAddr = ((stext as usize + etext as usize) / 2).into();
+    let mid_rodata: VirtAddr = ((srodata as usize + erodata as usize) / 2).into();
+    let mid_data: VirtAddr = ((sdata as usize + edata as usize) / 2).into();
+    assert_eq!(
+        kernel_space.page_table.translate(mid_text.floor()).unwrap().writable(),
+        false
+    );
+    assert_eq!(
+        kernel_space.page_table.translate(mid_rodata.floor()).unwrap().writable(),
+        false,
+    );
+    assert_eq!(
+        kernel_space.page_table.translate(mid_data.floor()).unwrap().executable(),
+        false,
+    );
+    println!("remap_test passed!");
+}
